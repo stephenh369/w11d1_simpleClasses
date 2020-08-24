c3a0169149473851b48224bf22bc927b88de238c
Stephen <stephen_h369@live.co.uk>
2020-08-24 16:16:48 +0100

    print and printEmpty tests pass

--- src/test/java/TestPrinter.java
+++ src/test/java/TestPrinter.java
@@ -5,10 +5,12 @@ import static org.junit.Assert.assertEquals;
 
 public class TestPrinter {
     Printer printer;
+    Printer emptyPrinter;
 
     @Before
     public void before() {
         printer = new Printer(50);
+        emptyPrinter = new Printer(0);
     }
 
     @Test
@@ -20,5 +22,13 @@ public class TestPrinter {
     public void print() {
         printer.print(3, 2);
         assertEquals(6, printer.getPrinted());
+        assertEquals(44, printer.getPaper());
+    }
+
+    @Test
+    public void printEmpty() {
+        emptyPrinter.print(3, 2);
+        assertEquals(0, emptyPrinter.getPrinted());
+        assertEquals(0, emptyPrinter.getPaper());
     }
 }
